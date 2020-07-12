1a1f8b846161467a20aaa8404421d299edf3fc57
yilihjy <yilihjy@gmail.com>
2020-07-12 15:42:55 +0800

    添加图片

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
         <img
           width="149"
           height="149"
-          src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149"
+          src="forkme_right_darkblue_121621.png"
           class="attachment-full size-full"
           alt="Fork me on GitHub"
           data-recalc-dims="1"
